49ce6c2e26f4a0a083a4d291dfd970ed52879bfb
Gabriel Barbosa <97404958+GBarbosa1@users.noreply.github.com>
2025-04-24 00:59:54 +0000

    Added debug prints

--- src/main.py
+++ src/main.py
@@ -38,6 +38,7 @@ def run_athena_query_df(
     return df
 
 def create_features(df):
+    print('Func entry',df)
     df['SMA_10'] = trend.sma_indicator(df['close'], window=10)
     df['SMA_50'] = trend.sma_indicator(df['close'], window=50)
     df['EMA_10'] = trend.ema_indicator(df['close'], window=10)
@@ -50,7 +51,9 @@ def create_features(df):
     df['ATR'] = volatility.average_true_range(df['high'], df['low'], df['close'], window=14)
     df['Bollinger_high'] = volatility.bollinger_hband(df['close'], window=20, window_dev=2)
     df['Bollinger_low'] = volatility.bollinger_lband(df['close'], window=20, window_dev=2)
+    print('After features',df)
     df.dropna(inplace=True)
+    print('After dropna',df)
     lag_days = 30
 
     for lag in range(1, lag_days + 1):
@@ -60,6 +63,7 @@ def create_features(df):
         df[f'Volume_Lag_{lag}'] = df['volume'].shift(lag)
 
     df['target'] = df['close'].shift(-1)
+    print('After lags',df)
     return df
 
 if __name__ == "__main__":
