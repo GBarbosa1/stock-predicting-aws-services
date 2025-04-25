efb7f63d0d794cf2c9e726a12751268630aeb347
Gabriel Barbosa <97404958+GBarbosa1@users.noreply.github.com>
2025-04-25 00:13:16 +0000

    Fix typo

--- src/main.py
+++ src/main.py
@@ -121,7 +121,7 @@ if __name__ == "__main__":
         data = create_features(data)
         data[feature_columns] = data[feature_columns].astype(float)
         
-        data.drop(columns=['date_capture','target'], inplace = Tru)
+        data.drop(columns=['date_capture','target'], inplace = True)
         print(data.columns)
         make_predictions(data[feature_columns].tail(10), 'src/xgboost/finance_xgboost.json')
         print(make_predictions)
